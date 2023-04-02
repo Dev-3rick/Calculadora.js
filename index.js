f21827c93c93056cd2b671c5593e05ebb9416410
Erick Henrique <erickhenrique.rg@gmail.com>
2023-04-02 18:07:47 -0300

    Calculadora	JS	primeira implementação '

---
+++ index.js
@@ -0,0 +1,91 @@
+const main = document.querySelector("main");
+const root = document.querySelector(":root");
+const input = document.getElementById("input");
+const resultInput = document.getElementById("result");
+
+const allowdKeys = [
+  "(",
+  ")",
+  "/",
+  "*",
+  "-",
+  "+",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  ".",
+  "%",
+  " ",
+];
+
+document.querySelectorAll(".charKey").forEach((charKeyBtn) => {
+  charKeyBtn.addEventListener("click", () => {
+    const value = charKeyBtn.dataset.value;
+    input.value += value;
+  });
+});
+
+document.getElementById("clear").addEventListener("click", () => {
+  input.value = "";
+  input.focus(); //Para focar no input.
+});
+
+input.addEventListener("keydown", (ev) => {
+  ev.preventDefault();
+  if (allowdKeys.includes(ev.key)) {
+    input.value += ev.key;
+    return;
+  }
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1);
+  }
+  if (ev.key === "Enter") {
+    calculate();
+  }
+});
+
+document.getElementById("equal").addEventListener("click", calculate);
+
+function calculate() {
+  resultInput.value = "ERROR";
+  resultInput.classList.add("error");
+  const result = eval(input.value);
+  resultInput.classList.remove("error");
+  resultInput.value = result;
+  console.log(result);
+}
+
+document.getElementById("copyToClipboard").addEventListener("click", (ev) => {
+  const button = ev.currentTarget;
+  if (button.innerText === "Copy") {
+    button.innerText = "Copied!";
+    button.classList.add("success");
+    navigator.clipboard.writeText(resultInput.value);
+  } else {
+    button.innerText = "Copy";
+    button.classList.remove("success");
+  }
+});
+
+document.getElementById("themeSwitcher").addEventListener("click", () => {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9");
+    root.style.setProperty("--border-color", "#aaa");
+    root.style.setProperty("--font-color", "#212529");
+    root.style.setProperty("--primary-color", "#26834a");
+    main.dataset.theme = "light";
+  } else {
+    root.style.setProperty("--bg-color", "#212529");
+    root.style.setProperty("--border-color", "#666");
+    root.style.setProperty("--font-color", "#f1f5f9");
+    root.style.setProperty("--primary-color", "#4dff91");
+    main.dataset.theme = "dark";
+  }
+});
