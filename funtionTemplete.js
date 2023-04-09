0a3f5b845ac8cadf7c27ac48f2306c4e601ecd80
Erick_Dev <erickhenrique.rg@gmail.com>
2023-04-09 02:19:10 -0300

    Aprendendo A ultilizar o export

---
+++ funtionTemplete.js
@@ -0,0 +1,18 @@
+export function templete(main, root) {
+  console.log(main.dataset.theme);
+
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9");
+    root.style.setProperty("--border-color", "#aaa");
+    root.style.setProperty("--font-color", "#212529");
+    root.style.setProperty("--primary-color", "#26834a");
+    main.dataset.theme = "light";
+  } else {
+    root.style.setProperty("--bg-color", "#212529");
+    root.style.setProperty("--border-color", "#666");
+    root.style.setProperty("--font-color", "#f1f5f9");
+    root.style.setProperty("--primary-color", "#4dff91");
+    main.dataset.theme = "dark";
+  }
+  console.log(main.dataset.theme);
+}
